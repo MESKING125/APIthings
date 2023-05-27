adf7fada19fc4637aac3086170a8af4887f37bfb
MESKING125 <marlen7891011@gmail.com>
2023-05-27 10:48:21 +0700

    a

---
+++ app/src/main/java/com/example/apithings/NewsAdapter.java
@@ -0,0 +1,4 @@
+package com.example.apithings;
+
+public class NewsAdapter {
+}
--- app/src/main/java/com/example/apithings/NewsModel.java
+++ app/src/main/java/com/example/apithings/NewsModel.java
@@ -1,5 +1,58 @@
 package com.example.apithings;
 
-public class NewsModel {
-    
-}
+ class NewsModel {
+    private String  source;
+    private String  author;
+    private String  title;
+    private String  desc;
+    private String  publish;
+    private String image;
+
+     public String getSource() {
+         return source;
+     }
+
+     public void setSource(String source) {
+         this.source = source;
+     }
+
+     public String getAuthor() {
+         return author;
+     }
+
+     public void setAuthor(String author) {
+         this.author = author;
+     }
+
+     public String getTitle() {
+         return title;
+     }
+
+     public void setTitle(String title) {
+         this.title = title;
+     }
+
+     public String getDesc() {
+         return desc;
+     }
+
+     public void setDesc(String desc) {
+         this.desc = desc;
+     }
+
+     public String getPublish() {
+         return publish;
+     }
+
+     public void setPublish(String publish) {
+         this.publish = publish;
+     }
+
+     public String getImage() {
+         return image;
+     }
+
+     public void setImage(String image) {
+         this.image = image;
+     }
+ }
